3e69537470b50e54828405f4f16d91504f78d4af
SNOWWWWWW4 <lescober7@gmail.com>
2024-05-04 11:15:36 -0700

    got the map and navbar to work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
-import Image from "next/image";
+'use client'
+import React from 'react'
+import BetaMap from './components/BetaMap';
+
 
 export default function Home() {
   return (
-    <main className='min-h-screen flex items-center bg-slate-600'>
-      <div>
-        <p>
-          hello
-        </p>
-        </div>
-    </main>
+    <>
+      
+        <BetaMap/>
+      
+    </>
   );
 }
